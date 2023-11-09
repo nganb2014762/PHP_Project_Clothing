fb83f0d8e1bfb8ef9a4dbc1bc8a35dba2617209e
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2023-11-09 22:31:33 +0700

    add_partials

---
+++ partials/header0.php
@@ -0,0 +1,118 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+    <div class="container">
+        <button class="navbar-toggler btn-primary d-lg-none border-0" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="offcanvas-lg offcanvas-start" tabindex="-1" id="offcanvasResponsive"
+            aria-labelledby="offcanvasResponsiveLabel">
+            <div class="offcanvas-header">
+                <a class="offcanvas-title nav-link text-uppercase fw-bold fs-4 px-3" href="index.php"
+                    id="offcanvasResponsiveLabel">
+                    home
+                </a>
+
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+                    data-bs-target="#offcanvasResponsive" aria-label="Close">
+                </button>
+            </div>
+            <hr>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav my-auto">
+                    <li class="nav-item dropdown px-2 py-2">
+                        <a class="nav-link text-uppercase text-dark" href="shop.php">shop</a>
+                    </li>
+                    <li class="nav-item px-2 py-2">
+                        <a class="nav-link text-uppercase text-dark" href="#">blogs</a>
+                    </li>
+                    <li class="nav-item px-2 py-2">
+                        <a class="nav-link text-uppercase text-dark" href="#">about us</a>
+                    </li>
+                    <li class="nav-item px-2 py-2">
+                        <a class="nav-link text-uppercase text-dark" href="contact.php">contact us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <a class="navbar-brand d-flex order-lg-1 justify-content-between align-content-center" href="index.php">
+            <img src="../img/logo.png" alt="site icon">
+        </a>
+
+        <div class="order-lg-2">
+            <div class="navbar-collapse nav-btns">
+                <div class="d-flex justify-content-between align-items-center">
+                    <span id="search" class="d-flex align-items-center me-2">
+                        <input type="text" placeholder="Tìm kiếm" class="form-control">
+                        <button type="button" class="btn position-relative">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                    <?php
+                    
+                    // session_start();
+                    // $select_profile = $pdo->prepare("SELECT * FROM `user` WHERE role = '0' ");
+                    // $select_profile->execute();
+                    // $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+                    
+                    // if(isset($fetch_profile)){
+                    //     echo ' <div class="dropdown">
+                    //             <a  style="color:#222; text-decoration: none;" href="#" role="button" data-bs-toggle="dropdown">
+                    //                 <i class="fa-solid fa-user"></i> <span>' . $fetch_profile['name'] . '</span>
+                    //             </a>
+                    
+                    //             <ul class="dropdown-menu">
+                    //                 <li><a class="dropdown-item" href="account.php">My Account</a></li>
+                    //                 <li><a id="logout" class="dropdown-item" href="logout.php">Đăng xuất</a></li>
+                    //             </ul>
+                    //         </div> ';
+                    // } else {
+                    //     echo '<button type="button" class="btn position-relative">
+                    //             <a href="login.php" style="color:#222;"><i class="fa fa-user"></i></a>
+                    //         </button>';
+                    // }
+
+                    // require_once __DIR__ . '../../partials/connect.php';
+                    // session_start();
+                    // $_SESSION['user_id'] = $user_id;
+                    require_once __DIR__ . '../../partials/connect.php';
+                    session_start();
+                    
+                    if (isset($_SESSION['user_id'])) {
+                        $user_id = $_SESSION['user_id'];
+                    
+                        $select_profile = $pdo->prepare("SELECT * FROM `user` WHERE id = ?");
+                        $select_profile->execute([$user_id]);
+                        $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+                    
+                        // Now, $fetch_profile contains the user's data
+                        echo ' <div class="dropdown">
+                                <a  style="color:#222; text-decoration: none;" href="#" role="button" data-bs-toggle="dropdown">
+                                    <i class="fa-solid fa-user"></i> <span>' . $fetch_profile['name'] . '</span>
+                                </a>
+                    
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="user_account.php">My Account</a></li>
+                                    <li><a class="dropdown-item" href="user_change_password.php">Change Password</a></li>
+                                    <li><a id="logout" class="dropdown-item" href="logout.php">Log Out</a></li>
+                                </ul>
+                            </div> ';
+                    } else {
+                        // Handle the case where the user is not logged in
+                        echo '<button type="button" class="btn position-relative">
+                                    <a href="login.php" style="color:#222;"><i class="fa fa-user"></i></a>
+                                </button>';
+                    }
+                    ?>
+                        <button class="btn position-relative" type="button">
+                            <a href="cart.php" style="color:#222;"><i class="fa fa-shopping-cart"></i></a>
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</nav>
+<!-- end of navbar -->
---
+++ partials/pagination.php
@@ -0,0 +1,13 @@
+<nav aria-label="Page navigation example" class="pt-5">
+    <ul class="pagination justify-content-end">
+        <li class="page-item disabled">
+            <a class="page-link text-dark">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link text-dark bg-success-subtle" href="#">1</a></li>
+        <!-- <li class="page-item"><a class="page-link text-dark bg-success-subtle" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link text-dark bg-success-subtle" href="#">3</a></li> -->
+        <li class="page-item">
+            <a class="page-link text-dark" href="#">Next</a>
+        </li>
+    </ul>
+</nav>
