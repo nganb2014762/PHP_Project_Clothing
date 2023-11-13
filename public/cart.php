2979e5dad71421a62410c4a82a91ac5b7c866d06
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2023-11-14 01:35:32 +0700

    cart

--- public/cart.php
+++ public/cart.php
@@ -27,15 +27,28 @@ if (isset($_GET['delete_all'])) {
 }
 ;
 
+// if (isset($_POST['update_qty'])) {
+//     $cart_id = $_POST['cart_id'];
+//     $p_qty = $_POST['p_qty'];
+//     $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+//     $update_qty = $pdo->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
+//     $update_qty->execute([$p_qty, $cart_id]);
+//     $message[] = 'cart quantity updated';
+// }
+// ;
+
 if (isset($_POST['update_qty'])) {
-    $cart_id = $_POST['cart_id'];
-    $p_qty = $_POST['p_qty'];
-    $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
-    $update_qty = $pdo->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
-    $update_qty->execute([$p_qty, $cart_id]);
-    $message[] = 'cart quantity updated';
+    $new_quantities = $_POST['p_qty'];
+    
+    foreach ($new_quantities as $cart_id => $new_quantity) {
+        // Kiểm tra nếu giá trị quantity hợp lệ (ví dụ: là một số dương)
+        if (is_numeric($new_quantity) && $new_quantity > 0) {
+            // Cập nhật quantity trong bảng cart
+            $update_qty = $pdo->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
+            $update_qty->execute([$new_quantity, $cart_id]);
+        }
+    }
 }
-;
 
 if (isset($message)) {
     foreach ($message as $message) {
@@ -53,107 +66,6 @@ if (isset($message)) {
 </head>
 
 <body>
-    <!-- Cart -->
-    <!-- <section id="cart" class="pt-5">
-        <div class="container">
-            <div class="title text-center mt-5 pt-5">
-                <h2 class="position-relative d-inline-block">Your Cart</h2>
-                <hr>
-            </div>
-
-            <?php
-            $total = 0;
-            $sub_total = 0;
-            $select_cart = $pdo->prepare("SELECT * FROM `cart` WHERE user_id = ?");
-            $select_cart->execute([$user_id]);
-            ?>
-            <?php if ($select_cart->rowCount() > 0) { ?>
-                <table class="mt-5 pt-5">
-                    <tr>
-                        <th>Product</th>
-                        <th>Quantity</th>
-                        <th>Subtotal</th>
-                    </tr>
-                    <?php
-                    while ($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)) {
-                        $product_subtotal = $fetch_cart['price'] * $fetch_cart['quantity'];
-                        $sub_total += $product_subtotal;
-                        ?>
-                        <tr>
-
-                            <td>
-                                <div class="product-info">
-                                    <img src="admin/uploaded_img/<?= htmlspecialchars($fetch_cart['image']); ?>" alt="">
-                                    <div>
-                                        <div class=" name text-capitalize">
-                                            <?= htmlspecialchars($fetch_cart['name']); ?>
-                                        </div>
-                                        <div class="price">
-                                            <?= htmlspecialchars($fetch_cart['price']); ?>$
-                                        </div>
-                                        <br>
-                                        <a class="text-capitalize text-align"
-                                            href="cart.php?delete=<?= htmlspecialchars($fetch_cart['id']); ?>"
-                                            onclick="return confirm('delete this from cart?');">remove</a>
-                                    </div>
-                                </div>
-                            </td>
-
-                            <td>
-                                <div class="quantity">
-                                    <?= htmlspecialchars($fetch_cart['quantity']); ?>
-                                </div>
-                            </td>
-
-                            <td>
-                                <span class="product-price">$
-                                    <?= htmlspecialchars($product_subtotal); ?>
-                                </span>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                    ?>
-                </table>
-
-
-                <div class="cart-total">
-                    <table>
-                        <tr>
-                            <td>Total</td>
-                            <td>$
-                                <?= $sub_total; ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Subtotal</td>
-                            <td><span>$
-                                    <?= $sub_total; ?>
-                                </span></td>
-                        </tr>
-                    </table>
-                </div>
-
-                <div class="checkout-container">
-                    <a class="btn checkout-btn" href="checkout.php">Checkout</a>
-                </div>
-
-                <?php
-            } else {
-                ?>
-                <div class="text-center pt-3">
-                    <h6 class="position-relative d-inline-block">No item found </h6>
-                    <div>
-                        <a type="submit" class="buy-btn text-capitalize text-decoration-none mt-3" name="shop now"
-                            href="shop.php">shop now</a>
-                    </div>
-                </div>
-                <?php
-            }
-            ?>
-        </div>
-    </section> -->
-
 
     <!-- Cart -->
     <section id="cart" class="pt-5">
@@ -170,9 +82,9 @@ if (isset($message)) {
             $select_cart->execute([$user_id]);
             ?>
             <?php if ($select_cart->rowCount() > 0) { ?>
-                <table class="mt-5 pt-5">
+                <table class="mt-5 pt-5 table-responsive">
                     <tr>
-                        <th class="col-2">Product</th>
+                        <th class="col-3">Product</th>
                         <th>Name</th>
                         <th>Price</th>
                         <th>Quantity</th>
@@ -184,45 +96,53 @@ if (isset($message)) {
                         $product_subtotal = $fetch_cart['price'] * $fetch_cart['quantity'];
                         $sub_total += $product_subtotal;
                         ?>
-                        <tr>
-
-                            <td>
-                                <div class="product-info">
-                                    <img src="admin/uploaded_img/<?= htmlspecialchars($fetch_cart['image']); ?>" alt=""
-                                        class="w-50">
-                                </div>
-                            </td>
-                            <td>
-                                <div class="product-name">
-                                    <div class=" name text-capitalize">
-                                        <?= htmlspecialchars($fetch_cart['name']); ?>
+                        <form method="POST" action="cart.php">
+                            <tr>
+                                <td>
+                                    <div class="product-info">
+                                        <img src="admin/uploaded_img/<?= htmlspecialchars($fetch_cart['image']); ?>" alt=""
+                                            class="w-50">
                                     </div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="product-price">
-                                    <div class="price">
-                                        <?= htmlspecialchars($fetch_cart['price']); ?>$
+                                </td>
+                                <td>
+                                    <div class="product-name">
+                                        <div class=" name text-capitalize">
+                                            <?= htmlspecialchars($fetch_cart['name']); ?>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="quantity">
-                                    <?= htmlspecialchars($fetch_cart['quantity']); ?>
-                                </div>
-                            </td>
-
-                            <td>
-                                <span class="product-price">$
-                                    <?= htmlspecialchars($product_subtotal); ?>
-                                </span>
-                            </td>
-                            <td>
-                                <a class="text-capitalize text-align"
-                                    href="cart.php?delete=<?= htmlspecialchars($fetch_cart['id']); ?>"
-                                    onclick="return confirm('delete this from cart?');">delete</a>
-                            </td>
-                        </tr>
+                                </td>
+                                <td>
+                                    <div class="product-price">
+                                        <div class="price">
+                                            <?= htmlspecialchars($fetch_cart['price']); ?>$
+                                        </div>
+                                    </div>
+                                </td>
+                                <!-- <td>
+                                    <div class="quantity">
+                                        <?= htmlspecialchars($fetch_cart['quantity']); ?>
+                                    </div>
+                                </td> -->
+                                <td>
+                                    <div class="quantity">
+                                        <input type="number" name="p_qty[<?= htmlspecialchars($fetch_cart['id']); ?>]"
+                                            value="<?= htmlspecialchars($fetch_cart['quantity']); ?>" min="1">
+                                    </div>
+                                    <button class="bg-white border-0 text-primary" type="submit"
+                                        name="update_qty">Update</button>
+                                </td>
+                                <td>
+                                    <span class="product-price">$
+                                        <?= htmlspecialchars($product_subtotal); ?>
+                                    </span>
+                                </td>
+                                <td>
+                                    <a class="text-capitalize text-align"
+                                        href="cart.php?delete=<?= htmlspecialchars($fetch_cart['id']); ?>"
+                                        onclick="return confirm('delete this from cart?');">delete</a>
+                                </td>
+                            </tr>
+                        </form>
                         <?php
                     }
                     ?>
@@ -267,8 +187,3 @@ if (isset($message)) {
     </section>
     <?php
     include_once __DIR__ . '../../partials/footer.php';
-    ?>
-
-</body>
-
-</html>
